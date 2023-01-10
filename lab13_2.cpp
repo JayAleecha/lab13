806e9be0c69f086fcc2b562b32341c2e1fec29fa
JayAleecha <aleecha.2546@gmail.com>
2023-01-10 16:09:57 +0700

    lab13_2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int k,int z){
+  if(k%z==0){ 
+     return z;
+    }else{ 
+     return gcd(z, k%z);
+}
 }
